78694d9339fef10cce74f504bce14bb1df6b7c65
Miruto80 <miguelfernando8012@gmail.com>
2025-02-11 18:28:24 -0400

    Agregando

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+let currentImageIndex = 0;
+const images = [
+    "imagenes/Last adds/TOYOTA 4RUNNER LIMITED NIGHTSHADE 4x4 2019.jpeg",
+    "imagenes/Last adds/HINO 2018 268 26 FT.jpeg"
+];
+const carouselBg = document.getElementById("carousel-bg");
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    carouselBg.src = images[currentImageIndex];
+}
+
+function previousImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    carouselBg.src = images[currentImageIndex];
+}
+
 document.getElementById('legal-button').addEventListener('click', function () {
     const legalContent = document.getElementById('legal-content');
     legalContent.classList.toggle('show');
